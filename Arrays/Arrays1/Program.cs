93764139cc1479a96452d6bdce38eef18162df0a
vanshika1421 <156989016+vanshika1421@users.noreply.github.com>
2026-07-26 15:56:55 +0530

    Update Program.cs

--- Arrays/Arrays1/Program.cs
+++ Arrays/Arrays1/Program.cs
@@ -1,1 +1,30 @@
-﻿Console.WriteLine("Hello, World!");
+using System;
+
+namespace Practice_Problems_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // CheckNumberIsPositiveNegativeZero.CheckNumbers();
+
+            // Copy_2D_array.CopyArray();
+
+            // EligibleToVote.CheckEligibility();
+
+            // FactorsOfNumber.FindFactors();
+
+            // FizzBuzz.FizzbuzzArray();
+
+            // MeanHeight.CalculateMeanHeight();
+
+            // MultiplicationTable.PrintTable();
+
+            // MultiplicationTableOfNumber.PrintMultiplicationTable();
+
+            // OddEven.PrintOddEven();
+
+            // SumOfNumbers.CalculateSum();
+        }
+    }
+}
