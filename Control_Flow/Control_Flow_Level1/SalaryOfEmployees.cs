1afd2d74940269cc787cbda50e08449427a9f7a2
vanshika1421 <156989016+vanshika1421@users.noreply.github.com>
2026-07-26 20:42:13 +0530

    Add EmployeeBonus method for calculating bonuses

--- Control_Flow/Control_Flow_Level1/SalaryOfEmployees.cs
+++ Control_Flow/Control_Flow_Level1/SalaryOfEmployees.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +6,23 @@ namespace Control_Flow
 {
     internal class SalaryOfEmployees
     {
+        public static void EmployeeBonus()
+        {
+            Console.Write("Enter Employee Salary: ");
+            double salary = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter Years of Service: ");
+            int yearsOfService = Convert.ToInt32(Console.ReadLine());
+
+            if (yearsOfService > 5)
+            {
+                double bonus = salary * 0.05;
+                Console.WriteLine("Bonus Amount: " + bonus);
+            }
+            else
+            {
+                Console.WriteLine("Employee is not eligible for bonus.");
+            }
+        }
     }
 }
