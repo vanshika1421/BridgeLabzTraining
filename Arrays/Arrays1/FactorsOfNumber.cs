53ae45720d1b89e17a95dc956bbfbce85f6224c4
vanshika1421 <156989016+vanshika1421@users.noreply.github.com>
2026-07-26 15:54:51 +0530

    Implement FindFactors method to calculate factors
    The method FindFactors prompts the user for a number and calculates its factors, dynamically resizing the array to store them. It includes input validation for positive numbers and displays the factors found.

--- Arrays/Arrays1/FactorsOfNumber.cs
+++ Arrays/Arrays1/FactorsOfNumber.cs
@@ -1,10 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System;
 
 namespace Practice_Problems_Array
 {
     internal class FactorsOfNumber
     {
+        public static void FindFactors()
+        {
+            Console.Write("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number <= 0)
+            {
+                Console.WriteLine("Please enter a positive number.");
+                return;
+            }
+
+            int maxFactor = 10;
+            int[] factors = new int[maxFactor];
+            int index = 0;
+
+            // Find factors
+            for (int i = 1; i <= number; i++)
+            {
+                if (number % i == 0)
+                {
+                    // Increase array size if full
+                    if (index == maxFactor)
+                    {
+                        maxFactor *= 2;
+                        int[] temp = new int[maxFactor];
+
+                        for (int j = 0; j < factors.Length; j++)
+                        {
+                            temp[j] = factors[j];
+                        }
+
+                        factors = temp;
+                    }
+
+                    factors[index] = i;
+                    index++;
+                }
+            }
+
+            // Display factors
+            Console.WriteLine("\nFactors of " + number + " are:");
+
+            for (int i = 0; i < index; i++)
+            {
+                Console.Write(factors[i] + " ");
+            }
+
+            Console.WriteLine();
+        }
     }
 }
