480b64e818cacd40644969bb9ce1c6737323dbdf
vanshika <vanshika1407@gmail.com>
2026-08-05 15:43:25 +0530

    Encapsulation and abstraction assignment added

---
+++ Encapsulation and abstraction/HospitalManagement.cs
@@ -0,0 +1,107 @@
+﻿//using Encapsulation;
+//using System.Runtime.Intrinsics.X86;
+
+//7.Hospital Patient Management
+//Description: Design a system to manage patients in a hospital.
+//Abstract Class:
+//Create an abstract class Patient with fields: patientId, name, and age.
+//Add an abstract method CalculateBill().
+//Implement a concrete method GetPatientDetails().
+//Subclasses:
+//Extend Patient into InPatient and OutPatient.
+//Implement CalculateBill() differently for each subclass.
+//Interface:
+//Implement an interface IMedicalRecord.
+//Define methods AddRecord() and ViewRecords().
+//Encapsulation:
+//Protect sensitive patient data like diagnosis and medical history.
+//Polymorphism:
+//Use a Patient reference to handle different patient types dynamically.
+//Display billing details based on polymorphic behavior.
+
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Encapsulation
+{
+   abstract internal class Patient
+    {
+        int patientId;
+        private string diagnosis { get; set; } = string.Empty;
+        private string medicalHistory { get; set; } = string.Empty;
+        string name;
+        int age;
+        public Patient(int patientId, string name, int age)
+        {
+            this.patientId = patientId;
+            this.name = name;
+            this.age = age;
+        }
+        protected void setDiagnoseAndMedicalRecord(string diagnosis , string medicalHistory)
+        {
+            this.diagnosis = diagnosis;
+            this.medicalHistory = medicalHistory;
+        }
+        protected void showDiagnoseAndMedicalRecord()
+        {
+            Console.WriteLine("Diagnosis: " + diagnosis);
+            Console.WriteLine("Medical History: " + medicalHistory);
+        }
+        public abstract int calculateBill();
+        public void getPateientsDetails()
+        {
+            Console.WriteLine("Patient ID: " + patientId);
+            Console.WriteLine("Patient Name: " + name);
+            Console.WriteLine("Patient Age: " + age);
+        }
+    }
+    internal class InPatient: Patient , IMedicalRecord
+    {
+        public InPatient(int patientId, string name, int age) : base(patientId, name, age)
+        {
+        }
+        public override int calculateBill()
+        {
+            return 1000;
+        }
+        public void AddRecord(string diagnosis, string medicalHistory)
+        {
+            setDiagnoseAndMedicalRecord(diagnosis, medicalHistory);
+            Console.WriteLine("Added successfully medical record for InPatient");
+        }
+        public void ViewRecords()
+        {
+            Console.WriteLine("Viewing medical records for InPatient");
+            showDiagnoseAndMedicalRecord();
+        }
+
+    }
+    internal class OutPatient : Patient , IMedicalRecord
+    {
+        public OutPatient(int patientId , string name , int age): base(patientId, name, age)
+        {
+
+        }
+        public void AddRecord(string diagnosis, string medicalHistory)
+        {
+            setDiagnoseAndMedicalRecord(diagnosis, medicalHistory   );
+            Console.WriteLine("Added successfully medical record for OutPatient");
+        }
+        public void ViewRecords()
+        {
+            Console.WriteLine("Viewing medical records for OutPatient");
+            showDiagnoseAndMedicalRecord();
+        }
+        public override int calculateBill()
+        {
+            return 1;
+        }
+    }
+    interface IMedicalRecord
+    {
+        void AddRecord(string diagnosis, string medicalHistory);
+        void ViewRecords();
+    }
+}
---
+++ Encapsulation/OnlineFoodOrdering.cs
@@ -0,0 +1,82 @@
+﻿
+//6.Online Food Delivery System
+//Description: Create an online food delivery system:
+//Define an abstract class FoodItem with fields like itemName, price, and quantity.
+//Add abstract methods CalculateTotalPrice() and concrete methods like GetItemDetails().
+//Extend it into classes VegItem and NonVegItem, overriding CalculateTotalPrice() to include additional charges.
+//Use an interface IDiscountable with methods ApplyDiscount() and GetDiscountDetails().
+//Use polymorphism to handle different types of food items dynamically.
+
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Encapsulation
+{
+
+    internal abstract class foodItems
+    {
+        public string itemName { get; private set; } = string.Empty;
+        public double Price { get; private set; }
+        public int Quantity { get; private set; }
+        public foodItems(string itemName , double Price , int Quantity) { 
+            this.itemName = itemName;
+            this.Price = Price;
+            this.Quantity = Quantity;
+        }
+        public abstract double CalculateTotalPrice();
+        public void getItemDetails()
+        {
+            Console.WriteLine($"Item Name : {itemName}");
+            Console.WriteLine($"Price : {Price}");
+            Console.WriteLine($"Quantity : {Quantity}");
+        }
+
+
+    }
+    internal class VegItems : foodItems , IDiscountable
+    {
+        public VegItems(string itemName , double Price , int Quantity) : base(itemName , Price , Quantity) { }
+        public override double CalculateTotalPrice()
+        {
+            return Price * Quantity + 100 ;
+        }
+        public double ApplyDiscount()
+        {
+            return 0.10 * (Price * Quantity);
+        }
+        public void GetDiscountDetails()
+        {
+            Console.WriteLine($"Discount of rupees {ApplyDiscount()} applied ");
+            Console.WriteLine($"Total Payable amount is now {CalculateTotalPrice() - ApplyDiscount()}");
+        }
+
+    }
+    internal class NonVegItems : foodItems , IDiscountable
+    {
+        public NonVegItems(string itemName, double Price, int Quantity) : base(itemName, Price, Quantity)
+        {
+        }
+
+        public override double CalculateTotalPrice()
+        {
+            return Price * Quantity + 200;
+        }
+        public double ApplyDiscount()
+        {
+            return Price * Quantity * 0.05;
+        }
+        public void GetDiscountDetails()
+        {
+            Console.WriteLine($"Discount of rupees {ApplyDiscount()} is applied" );
+            Console.WriteLine($"Now payable amount is {CalculateTotalPrice() - ApplyDiscount()}");
+        }
+    }
+    internal interface IDiscountable
+    {
+        double ApplyDiscount();
+        void GetDiscountDetails();
+    }
+}
+
