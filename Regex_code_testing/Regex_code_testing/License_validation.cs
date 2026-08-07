a0bd2a7e1f45f2e67394f7660f819e810943ac1b
vanshika <vanshika1407@gmail.com>
2026-08-07 20:51:14 +0530

    Regex assignment added

---
+++ Regex_code_testing/Regex_code_testing/License_validation.cs
@@ -0,0 +1,28 @@
+﻿//2.Validate a License Plate Number
+//License plate format: Starts with two uppercase letters, followed by four digits.
+//Example: "AB1234" is valid, but "A12345" is invalid.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using regex_code;
+namespace Regex_code_testing
+{
+    internal class License_validation
+    {
+        private License_validator lv;
+        [SetUp]
+        public void setUp()
+        {
+            lv = new License_validator();
+        }
+        [Test]
+        public void testing_License_Validation()
+        {
+            string str = "AB1234";
+            Assert.That(lv.License_validate(str), Is.True);
+        }
+
+
+    }
+}
---
+++ Regex_code_testing/Regex_code_testing/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using regex_code;
+namespace Regex_code_testing
+
+{
+    public class Tests
+    {
+        private UserName_Validator uv;
+        [SetUp]
+        public void Setup()
+        {
+            uv = new UserName_Validator();
+        }
+
+        [Test]
+        public void testing_username_validation()
+        {
+
+            string str = "user_123";
+            Assert.That(uv.username_valid(str), Is.True);
+        }
+    }
+}
