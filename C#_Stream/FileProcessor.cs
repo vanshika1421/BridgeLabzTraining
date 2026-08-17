2ed561e5c652c67067f8b8b71f51eeed8bb25d5f
vanshika <vanshika1407@gmail.com>
2026-08-17 21:36:16 +0530

    Streams added

---
+++ C#_Stream/FileProcessor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+public class FileProcessor
+{
+    public void CopyFile(string sourceFile, string destinationFile)
+    {
+        try
+        {
+            using (FileStream sourceStream = new FileStream(
+                sourceFile,
+                FileMode.Open,
+                FileAccess.Read))
+            {
+                using (FileStream destinationStream = new FileStream(
+                    destinationFile,
+                    FileMode.Create,
+                    FileAccess.Write))
+                {
+                    int data;
+
+                    while ((data = sourceStream.ReadByte()) != -1)
+                    {
+                        destinationStream.WriteByte((byte)data);
+                    }
+                }
+
+            }
+
+            Console.WriteLine("File copied successfully.");
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("Source file does not exist.");
+        }
+        catch (IOException ex)
+        {
+            Console.WriteLine("File error: " + ex.Message);
+        }
+    }
+}
---
+++ C#_Stream/Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        FileProcessor processor = new FileProcessor();
+
+        processor.CopyFile(
+            "source.txt",
+            "destination.txt"
+        );
+    }
+}
