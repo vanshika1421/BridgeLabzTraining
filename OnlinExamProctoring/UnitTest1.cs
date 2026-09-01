d6298a4b7c50ca174c1c82c2bf82f564ed74942a
vanshika1421 <156989016+vanshika1421@users.noreply.github.com>
2026-09-01 14:37:38 +0530

    Refactor unit tests for score and integrity rules

--- OnlinExamProctoring/UnitTest1.cs
+++ OnlinExamProctoring/UnitTest1.cs
@@ -1,27 +1,55 @@
-﻿
-using OnlinExamProctoring;
-namespace OnlineExam_testing
-{
-    public class Tests
-    {
+using NUnit.Framework;
+using System;
 
-        private ExamGradingEngine e;
-        [SetUp]
-        public void Setup()
+namespace OnlinExamProctoring.Tests
+{
+    public class UnitTest1
+    {
+        [Test]
+        public void CreateScore_ReturnsCorrectScore()
         {
-            e = new ExamGradingEngine();
+            Submissions submission =
+                new Submissions(1, 0, false);
+
+            var scoreRule =
+                Submissions.CreateScore(0.2);
+
+            double result = scoreRule(submission);
+
+            Assert.That(result, Is.EqualTo(0.8));
         }
 
         [Test]
-        public void TestingClassAvgScore()
+        public void IntegrityRule_ReturnsFalse_ForTooManyTabSwitches()
         {
-            int Total = 0;
-            int res = 5000 / Total;
+            ExamGradingEngine engine =
+                new ExamGradingEngine();
 
-            Assert.Throws<DivideByZeroException>(() =>
-            {
-                
-            });
+            Submissions submission =
+                new Submissions(
+                    1,
+                    DateTime.Now.AddMinutes(-10),
+                    DateTime.Now,
+                    10);
+
+            var rule =
+                engine.CreateIntegrityRule(
+                    TimeSpan.FromMinutes(5),
+                    5);
+
+            Assert.That(rule(submission), Is.False);
+        }
+
+        [Test]
+        public void ComputingClassAverage_ReturnsCorrectAverage()
+        {
+            ExamGradingEngine engine =
+                new ExamGradingEngine();
+
+            int result =
+                engine.ComputingClassAverageScore(10, 800);
+
+            Assert.That(result, Is.EqualTo(80));
         }
     }
 }
